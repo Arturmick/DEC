852b6d8fcccd17a232ac359cc866dcd659cc9919
Arturmi <artur.trombo@gmail.com>
2024-10-21 14:46:34 +0200

    case

---
+++ pelotas/cronometro.js
@@ -0,0 +1,74 @@
+let horas = 0;
+
+let minutos = 0;
+
+let segundos = 0;
+
+let cronometro;
+
+window.onload = function() {
+    cronometro = document.getElementById('cronometro');
+
+    document.getElementById('jugar').addEventListener('click',cronometrar);
+}
+
+/**********************CRONOMETRO***************************/
+
+//Comienza a cronometrar
+
+
+function cronometrar(){
+
+    crearReloj();
+
+    intervalo = setInterval(crearReloj,1000);
+
+}
+
+function crearReloj(){
+
+    let hAux, mAux, sAux;
+
+    segundos++;
+
+    if (segundos>59) {
+
+        minutos++;
+
+        segundos=0;
+
+    }
+
+    if (minutos>59) {
+
+        horas++;
+
+        minutos=0;
+
+    }
+
+    if (horas>24) horas=0;
+
+    if (segundos<10) sAux="0"+segundos;
+
+    else sAux=segundos;
+
+    if (minutos<10) mAux="0"+minutos;
+
+    else mAux=minutos;
+
+    if (horas<10) hAux="0"+horas;
+
+    else hAux=horas;
+
+    cronometro.innerText = hAux + ":" + mAux + ":" + sAux;
+
+}
+
+//Detiene el cronometro
+
+function parar(){
+
+    clearInterval(intervalo);
+
+}
