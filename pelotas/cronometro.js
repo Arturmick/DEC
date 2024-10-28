5fbd3ca7435bc3ed8b3f7542c49ba31211b64ea9
Arturmi <artur.trombo@gmail.com>
2024-10-28 14:41:03 +0100

    clase

--- pelotas/cronometro.js
+++ pelotas/cronometro.js
@@ -1,5 +1,9 @@
-let numeroBolas = 10;
+let numeroBolasVar = 10;
 let circulos;
+let juegoVar;
+let menuColor = document.createElement('div');
+let colorPelotas;
+
 
 let horas = 0;
 let minutos = 0;
@@ -16,65 +20,148 @@ window.onload = function() {
 function cargarEventos() {
 
     document.getElementById('jugar').addEventListener('click',juego);    
-    document.getElementById('eliminar1').addEventListener('click',cambiarColorBoton);
-    document.getElementById('eliminar2').addEventListener('click',cambiarColorBoton);
-    document.getElementById('numeroBolas').addEventListener('change',bolas);
-    for (let i = 0; i < circulos.length; i++) {
-        circulos[i].addEventListener('dblclick', eliminarBola);
-    }    
+    document.getElementById('eliminarTodo').addEventListener('click',cambiarColorBoton);
+    document.getElementById('eliminarColor').addEventListener('click',cambiarColorBoton);
+            
 }
 
 function juego() {
+    bolas();
+
     cronometrar();
+
+    if(eliminarTodo.classList.contains("seleccionado")) {
+        juegoVar = 1;
+    } else {
+        juegoVar = 2;
+    }
+
+      
+
+    
+    
     menu.remove();
-    creaCirculos();    
+
+    elegirColor();
+    
+    creaCirculos();
+    
+    for (let i = 0; i < circulos.length; i++) {
+        circulos[i].addEventListener('dblclick', eliminarBola);
+        circulos[i].addEventListener('dblclick', jugando);
+    }
+    
 }
+/********************** FUNCIONALIDAD JUEGOS **********************/
+
+function jugando(){
+    
+    if(juegoVar == 1) {
+        juegoEliminarTodo();
+    }else {
+        juegoEliminarColor(this);
+    }    
+}
+
+function juegoEliminarTodo() {
+    
+    if(circulos.length == 0) {
+        parar();
+        alert("¡Felicidades! Has ganado en " + cronometro.innerText + ".");
+    }
+      
+}   
+
+function juegoEliminarColor(boton) {
+
+    if (boton.classList.contains('puntuable')) {
+        marcadorRojo.innerText = parseInt(marcadorRojo.innerText) + 1;
+    }
+
+    let puntuable = document.getElementsByClassName('puntuable');
+
+    if(puntuable.length == 0) {
+        parar();
+        alert("¡Felicidades! Has ganado en " + cronometro.innerText + ".");
+    }
+}
+
+/********************** FUNCIONALIDAD BOTONES ******************/
 
 function cambiarColorBoton() {
     this.classList.remove("seleccionado");
     this.classList.add("seleccionado");
-    if(this.id == "eliminar1") {
-        document.getElementById('eliminar2').classList.remove("seleccionado");
+    if(this.id == "eliminarTodo") {
+        document.getElementById('eliminarColor').classList.remove("seleccionado");
     }else {
-        document.getElementById('eliminar1').classList.remove("seleccionado");
+        document.getElementById('eliminarTodo').classList.remove("seleccionado");
     }
 }   
 
-/********************CREA PELOTAS **************************/
+/******************** CREA PELOTAS **************************/
 
 function bolas(){
-    numeroBolas = parseInt(numeroBolas.value);
+    numeroBolasVar = numeroBolas.value;
 } 
 
-function creaCirculos(){
+function elegirColor() {
     
-    for (let i = 0; i < numeroBolas; i++) {
-        console.log("entra");
+    menuColor.innerHTML = "<div class='cartel'>ELIGE UN COLOR: </div><div id='botonRojo'>Rojo</div>" +
+                      "<div id='botonAzul'>Azul</div>";
+    menuColor.classList.add('menuColor');
+    areaJuego.appendChild(menuColor);
+    
+    document.getElementById('botonRojo').addEventListener('click', colorPelotas = 1);
+    document.getElementById('botonAzul').addEventListener('click', colorPelotas = 2);
+   
+}
+function creaCirculos(){
+
+    for (let i = 0; i < numeroBolasVar; i++) {
+        
         let circulo = document.createElement('div');
         
         circulo.classList.add('circulo');
 
-        let x = parseInt(Math.random() * (areaJuego.clientWidth - 50));
-        let y = parseInt(Math.random() * (areaJuego.clientHeight - 50));
-
-       //alto = ancho = parseInt(Math.random(10,50));
+        let x = parseInt(Math.random() * (areaJuego.clientWidth - 70));
+        let y = parseInt(Math.random() * (areaJuego.clientHeight - 70));
+        
+        alto = ancho = parseInt(Math.random() * 45) + 30;
 
         circulo.style.left = `${x}px`;
-        circulo.style.top = `${y}px`;
-        //circulo.style.width = `${alto}px`;
-        //circulo.style.height = `${ancho}px`;
+            circulo.style.top = `${y}px`;
+            circulo.style.width = `${alto}px`;
+            circulo.style.height = `${ancho}px`;
+         
+        if(juegoVar == 2 && colorPelotas == 1) {
 
-        areaJuego.appendChild(circulo); 
-    }  
+            let randomColor = Math.random() < 0.2 ? 'red' : "rgb(0, "+ (Math.random()*255) +", "+ (Math.random()*255) +")";
+            circulo.style.backgroundColor = randomColor;
+
+            
+            if (randomColor === 'red') {
+            circulo.classList.add('puntuable');
+            }
+
+        } else if(juegoVar == 2 && colorPelotas == 2) {
+            let randomColor = Math.random() < 0.2 ? 'blue' : "rgb("+ (Math.random()*255) +", "+ (Math.random()*255) +", 0)";
+            circulo.style.backgroundColor = randomColor;
+
+            if (randomColor === 'blue') {
+                circulo.classList.add('puntuable');
+            }
+        }
+        areaJuego.appendChild(circulo);  
+    }      
 }
 
-/**********************ELIMINA PELOTAS **********************/
+/********************** ELIMINA PELOTAS **********************/
 
 function eliminarBola(){
     this.remove();
 }
 
-/**********************CRONOMETRO***************************/
+/********************** CRONOMETRO***************************/
 
 //Comienza a cronometrar
 
